f8804cd092ab4c8d1e56032ac6fb4c2df6a70ae4
muzea <mr.muzea@gmail.com>
2020-06-12 11:30:51 +0800

    test: moment dependency

--- src/mount.tsx
+++ src/mount.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { render } from "react-dom";
+import moment from "moment";
 import APP from "./App";
 import "./style.css";
+
+console.log(moment().format('YYYY'))
 
 render(<APP />, document.querySelector("#app"));
